3edb48241e45563e389532fff37e79bbd8bcfe1d
Tu FirOcT <hongtupharma@gmail.com>
2023-08-26 17:01:33 +0700

    fix vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/mmURL-vueTS/',
+  base: '/urlShortener-vue/',
   plugins: [
     vue(),
   ],
